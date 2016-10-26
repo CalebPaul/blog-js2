6e51e5e864b186dcca2c6ea477bed1a86811e25b
Caleb Paul and Marta Sánchez <marta@>
2016-10-26 14:53:24 -0700

    allow the user to create a new blogpost

---
+++ app/components/new-post.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    saveNewPost(){
+      var params = {
+        title: this.get('title'),
+        imageFeature: this.get('imageFeature'),
+        imageContent: this.get('imageContent'),
+        teaser: this.get('teaser'),
+        content: this.get('content'),
+        timestamp: this.get('timestamp'),
+        author: this.get('author'),
+      };
+      this.sendAction('saveNewPost', params);
+    }
+  }
+});
