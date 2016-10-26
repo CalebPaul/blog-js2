6060b9f53110b01445553b41d8ec4e7a34190a15
Caleb Paul and Marta Sánchez <caleb@>
2016-10-26 16:48:39 -0700

    add a remove button

--- app/components/admin-tile.js
+++ app/components/admin-tile.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  delete(rental) {
+     if (confirm('Are you sure you want to delete this post?')) {
+       this.sendAction('destroyPost', post);
+     }
 });
