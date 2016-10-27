e25caf32e97adc0ea1bce36231ce350c520588fb
Caleb Paul and Marta Sánchez <caleb@>
2016-10-27 09:32:55 -0700

    add hbs update form, fix syntax errors

--- app/components/admin-tile.js
+++ app/components/admin-tile.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  delete(rental) {
-     if (confirm('Are you sure you want to delete this post?')) {
-       this.sendAction('destroyPost', post);
-     }
+  actions: {
+    delete(post) {
+      if (confirm('Are you sure you want to delete this post?')) {
+        this.sendAction('destroyPost', post);
+      }
+    }
+  }
 });
